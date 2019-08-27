07c1a0f2e9b47e5b2bb5cc16f7676e95ddaddac5
Erik Hanchett <erik.hanchett@gmail.com>
2019-08-26 20:41:52 -0700

    updated page

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,12 @@
     <div>
       <logo />
       <h1 class="title">
-        nuxtjs
+		DevBootCamp.io
       </h1>
       <h2 class="subtitle">
-        My astonishing Nuxt.js project
+		Coming Soon
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
       </div>
     </div>
   </div>
