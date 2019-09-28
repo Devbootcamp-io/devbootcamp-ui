637b9d1d416090800bf818edb0d29b3944adab69
Erik Hanchett <erik.hanchett@gmail.com>
2019-09-28 13:18:51 -0700

    updated favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "Developer Bootcamp Resource!",
     meta: [
       { charset: "utf-8" },
       {
